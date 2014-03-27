01586737f04551288b7c3871fe2d3118f2c41153
Carlo <carlo@phpbbitalia.net>
2014-03-27 18:07:41 +0100

    Base extension class

---
+++ ext.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @package SEO Search Tags
+ * @copyright (c) 2013 Carlo (carlino1994/carlo94it)
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace carlo94it\seosearchtags;
+
+class ext extends \phpbb\extension\base
+{
+}
